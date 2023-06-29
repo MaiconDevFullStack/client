6c4df9bb1bab65dbf8e2389bc7cea65f431058e1
maico <maico@192.168.0.106>
2023-06-29 01:37:59 -0300

    Scope screen finish
    -Functions created and screen scope for the others screen was finished.

--- webapp/include/js/controller/myAppController.js
+++ webapp/include/js/controller/myAppController.js
@@ -3,17 +3,26 @@ var app = angular.module("myApp", []);
 	
 	$scope.cities = [];
 	$scope.modalMessage = null;
+	$scope.error = false;
+	$scope.modalMessageObject = null;
+	$scope.modalTitle = null;
 	
 	refresh();
 	
 	function refresh(){
 		$http.get("http://localhost:3001/City").then(function(response){
 			$scope.cities = response.data;
+			$scope.modalMessage = null;
+			$scope.error = false;
 		});
 	}
 	
 	$scope.btnInsert = function(){
 		$scope.city = {};
+		$scope.modalMessage = null;
+		$scope.error = false;
+		$scope.selectedObject = null;
+		$scope.modalTitle = 'Insert City';
 		$(document).ready(function() {
 			$('#insertModal').modal();
 		});
@@ -21,16 +30,19 @@ var app = angular.module("myApp", []);
 	
 	$scope.insertCity = function(city){
 		if(!city.name){
+			$scope.error = true;
 			$scope.modalMessage = 'Please Insert the Name!';
 			return;
 		}
 		if(!city.size){
+			$scope.error = true;
 			$scope.modalMessage = 'Please Insert the Size!';
 			return;
 		}
 		$http.post("http://localhost:3001/City", city).then(function(){
 			delete $scope.city;
 			$scope.selectedObject = null;
+			$scope.modalTitle = null;
 			$('#insertModal').modal('hide');
 			refresh();
 		});
@@ -44,7 +56,9 @@ var app = angular.module("myApp", []);
 			$scope.modalMessage = 'Please Select At Least One Record!';
 			return;
 		} else {
-			$scope.modalMessage = 'Do You Really Want Remove The Record?!';
+			$scope.modalMessage = 'Do You Really Want Remove The Record?';
+			$scope.modalTitle = 'Delete City';
+			$scope.modalMessageObject = $scope.selectedObject.name;
 			$(document).ready(function() {
 				$('#deleteModal').modal();
 			});
@@ -55,6 +69,8 @@ var app = angular.module("myApp", []);
 		var parameter = $scope.selectedObject.id; 
 		$http.delete("http://localhost:3001/City/"+parameter).then(function(){
 			$scope.selectedObject = null;
+			$scope.modalTitle = null;
+			$scope.modalMessageObject = null;
 			$('#deleteModal').modal('hide');
 			refresh();	
 		});
@@ -62,8 +78,20 @@ var app = angular.module("myApp", []);
 	
 	$scope.edit = function(city){
 		$scope.city = angular.copy(city);
+		$scope.modalTitle = 'Edit City';
 		$(document).ready(function() {
 			$('#editModal').modal();
+		});
+	}
+	
+	$scope.confirmEdit = function(){
+		var parameter = $scope.selectedObject.id;
+		var parameter2 = $scope.city;
+		$http.patch("http://localhost:3001/City/"+parameter, parameter2).then(function(){
+			$scope.selectedObject = null;
+			$scope.modalTitle = null;
+			$('#editModal').modal('hide');
+			refresh();	
 		});
 	}
 	
