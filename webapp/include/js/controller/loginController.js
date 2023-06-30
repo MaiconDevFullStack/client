7e2bd89eb3e0aee50831bd19a9e7b2adf00a79b6
maico <maico@192.168.0.106>
2023-06-30 01:03:56 -0300

    Create Login Screen
    -Login screen was created and initial functions are made.

--- webapp/include/js/controller/loginController.js
+++ webapp/include/js/controller/loginController.js
@@ -1,4 +1,19 @@
 var app = angular.module('login', []);
 app.controller('loginController', function($scope, $http){
 	
+	$scope.acess = [{user:"MAICON", password:"123"},{user:"JOAO", password:"123"}];
+	
+	$scope.validLogin = function(ac){
+		for(a in $scope.acess){
+			if(ac.user === $scope.acess[a].user && ac.password === $scope.acess[a].password){
+				console.log("ENTER");
+				return;
+			}
+			else {
+				console.log("ERROR");
+				return;
+			}
+		}
+	}
+	
 });
