7e38eb346fc7b13c325adf2f3e94fcf589378324
Desenvolvimento 2 <maiconeluiz@hotmail.com>
2023-07-11 18:12:56 -0300

    Adjustments in cash Screen
    -Sale saving on database, doing the validations.

--- webapp/include/js/controller/cashController.js
+++ webapp/include/js/controller/cashController.js
@@ -1,7 +1,11 @@
 var app = angular.module('cash',['recordProduct']).controller('cashController',['$scope', 'cashAPI', 'productAPI', function($scope, cashAPI, productAPI){
 	
 	$scope.products = [];
-	$scope.sale = [];
+	$scope.sale = {};
+	$scope.sale.product = [];
+	$scope.modalTitle = null;
+	$scope.modalMessage = null;
+	$scope.modalObject = null;
 	
 	retrieveProducts();
 	
@@ -14,18 +18,41 @@ var app = angular.module('cash',['recordProduct']).controller('cashController',[
 	//IMPLEMENTNG SALE
 	$scope.btnInsert = function(codeBar){
 		
-		$scope.totalSale = 0;
+		$scope.sale.totalSale = 0;
 		
 		for(a in $scope.products){
 			if(codeBar == $scope.products[a].codeBar){
-				$scope.sale.push($scope.products[a]);
-				console.log($scope.sale);
+				$scope.sale.product.push($scope.products[a]);
 			}	
 		}
 		
-		for(b in $scope.sale){
-			$scope.totalSale = $scope.totalSale + $scope.sale[b].costSale; 
+		for(b in $scope.sale.product){
+			$scope.sale.totalSale = $scope.sale.totalSale + $scope.sale.product[b].costSale; 
 		}
+	}
+	
+	$scope.btnFinishSale = function(){
+		$scope.modalMessage = 'Do You Really Want Finish This Sale?';
+		$scope.modalObject = 'Total'
+		$scope.modalTitle = 'Finish Sale';
+		$(document).ready(function() {
+			$('#finishModal').modal();
+		});
+	};
+	
+	$scope.finishSale = function(){
+		$scope.sale.saleDate = new Date();
+		
+		cashAPI.post($scope.sale).then(function(){
+			console.log($scope.sale);
+			$scope.modalMessage = null;
+			$scope.modalObject = null;
+			$scope.modalTitle = null;
+			$('#finishModal').modal('hide');
+		});
+		$scope.sale = null;
+		iBarCode = null;
+		document.getElementById("iBarCode").focus();
 	}	
 	
 	///////////////////////////////////////////
