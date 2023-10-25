b4181ee451d5e317039ddf2005f60f20bd640466
Desenvolvimento 2 <maiconeluiz@hotmail.com>
2023-10-25 18:00:53 -0300

    Implementing Websocket

--- webapp/app/server.js
+++ webapp/app/server.js
@@ -3,6 +3,25 @@ const cors = require('cors');
 const app = express();
 const upload = require('express-fileupload');
 const net = require('net');
+const db = require("../app/models");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8081;
+const PORT2 = process.env.PORT || 8082;
+const IP = 'localhost';
+
+
+
+const handleConnection = socket =>{
+	console.log('Device Connected');
+	socket.on('end', ()=>{
+		console.log('Device Disconected')
+	});
+	socket.on('data', data=>{
+		console.log(data.toString());
+	});
+}
+
+const server = net.createServer(handleConnection);
 
 app.use(upload());
 
@@ -13,8 +32,6 @@ app.use(cors({
 app.use(cors({
     methods: ['GET','POST','DELETE','UPDATE','PUT']
 }));
-
-const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
@@ -29,34 +46,16 @@ require("../app/routes/service.routes.js")(app);
 require("../app/routes/sale.routes.js")(app);
 require("../app/routes/dashBoard.routes")(app);
 
-const handleConnection = socket =>{
-	console.log('Device Connected');
-	socket.on('end', ()=>{
-		console.log('Device Disconected')
-	});
-	socket.on('data', data=>{
-		console.log(data.toString());
-	});
-}
-
-const server = net.createServer(handleConnection);
-
-
 //SET THE SOCKET SERVER CONFIGURATIONS
-const PORT2 = process.env.PORT || 8082;
-const IP = 'localhost';
 server.listen(PORT2, IP,() => {
 	console.log(`Server SOCKET is running in http://${IP}:${PORT2}`);
 })
 
 //SET THE SERVICE ENDPOINTS CONFIGURATIONS
-const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server ROUTES is running in http://${IP}:${PORT}`);
 });
 
-
-const db = require("../app/models");
 
 db.sequelize.sync().then(() => {
   	console.log("Sync db");
--- webapp/include/js/controller/rootController.js
+++ webapp/include/js/controller/rootController.js
@@ -1,6 +1,8 @@
 var app = angular.module("root",[]).controller("rootController", ["$scope", '$rootScope', function($scope, $rootScope){
+	$scope.userLog = $rootScope.userLog
+	
 	$scope.location = function(){
 		location.href ="index.html";
 	}
-	console.log($rootScope.userLog);
+	console.log($scope.userLog);
 }]);
